0082cc74f8024388fbe001956c3154a5ca900a5a
MrAnderson64 <mranderson64@live.com>
2020-10-17 02:50:01 +0100

    page and initial template setup

---
+++ templates/serps.php
@@ -0,0 +1,15 @@
+<?php
+$top = '<div class="container"><h1 class="tc grey">' . $contentTitle . '</h1>';
+$middle = '
+<link href="' . $host . '/google.css" rel="stylesheet">
+<div class="google">
+	<cite>liamanderson.co.uk</cite>
+	<h3>Liam Anderson - Profile, Portfolio and Project</h3>
+	<p class="d">Liam Anderson Full-Stack Web developer | Lancashire, Lancaster</p>
+</div>
+';
+$bottom = '</div>';
+
+$templateOutput = $top . $middle . $bottom;
+
+?>
